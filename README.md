f711db0c9b51fb221690647957c151cc7d09b5ff
Ezharjan <ezharjan@outlook.com>
2023-04-02 00:13:18 +0800

    -

--- README.md
+++
@@ -1,8 +0,0 @@
-# Avatar Decorator
-
-头像装饰器
-
-如需定制头像框和挂件，修改src/heads.js即可
-
-### License
-MIT
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'element-ui/lib/theme-chalk/base.css';
-import 'mdn-polyfills/HTMLCanvasElement.prototype.toBlob';
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
